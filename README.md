0ccd123f71f0fd40dc6f95521ecd31f2bc455823
NECfukuoka <k-fukuoka@cd.jp.nec.com>
2015-12-01 20:15:13 +0900

    評価未完了の暫定版となります。

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# internal-counter
-[work in progress] internal-counter
+# カウンタ機能
+
+----
+## 概要
+カウンタ機能は，ページの累計アクセス数を加算し，その累計情報を配信するためのWeb ReST APIです．
+PHPで実装され，Apache HTTPDサーバ経由で配信されます．このアプリケーションはDockerで起動します．
+検索パラメータをURLに含めてGETメソッドでリクエストを送信すると，検索結果がJSON形式で戻ります．
+
+
+----
+### Dockerへの配備方法
+
+1. GitHubよりZIPファイルとして本API一式をダウンロードし，Dockerの環境にコピーし，ZIPを展開します．
+
+2. 展開したフォルダ内直下(counter)に移動し，
+以下のdockerコマンドを実行しDocker Imageを作成します．
+```
+$ docker build -t counter .
+```
+
+3. Docker Containerを配備(起動)します．
+```
+$ docker run -itd -p 8083:80 --name counter counter
+```
+**ポート番号の部分は自身の環境にあわせて変更してください．**
+
+4. 以下のURLにアクセスし，JSONが戻ることを確認します．
+```
+http://localhost:8083/CounterJson.php
+```
+**localhostではなくIPアドレスを指定してもかまいません**
+
+**リクエストしたクライアントのRefererによりカウントを制限しています。自身のサイトで使用する場合は、CounterJson.phpで定義しているACCEPTABLE_REFERERを自身のサイトのURLに変更します。**
+
+
+
+----
+## API仕様
+
+APIへのリクエスト(URL)は以下となります：
+```
+http://[server]/CounterJson.php
+```
+
+
+レスポンスとしてJSON形式の値が戻ります．フォーマットは以下のようになります：
+```
+{"results":{"total":[全体の総計],"today":[今日(現時点)の総計],"yesterday":[昨日の総計]}}
+```
+
+例：リクエスト
+
+```
+http://localhost:8083/CounterJson.php
+```
+
+例：レスポンス(JSON)
+
+```
+{"results":{"total":7,"today":3,"yesterday":4}}
+```
---
+++ src/CounterJson.php
@@ -0,0 +1,97 @@
+<?php
+define("ACCEPTABLE_REFERER","http://staging.maps.gsi.go.jp/");
+define("COUNTER_LOG", "/var/log/apache2/jsgi-map-counter.txt");
+
+counterAPI();
+/**
+ * カウンターAPI
+ */
+function counterAPI() {
+	if (preg_match('/\b(gzip|deflate)\b/i', $_SERVER['HTTP_ACCEPT_ENCODING'], $matches)) {
+		// 圧縮可能であれば圧縮する
+		ini_set("zlib.output_compression","On");
+	}
+	if (defined("ACCEPTABLE_REFERER") && !isset($_SERVER["HTTP_REFERER"])) {
+		// リファラーが設定されているのに、リクエストにリファラーが無い
+		sendResult();
+		return;
+	}
+	$referer = $_SERVER["HTTP_REFERER"];
+	if (defined("ACCEPTABLE_REFERER") && strpos($referer,ACCEPTABLE_REFERER) === FALSE) {
+		// 設定されているリファラーがとリクエストにリファラーが一致しない
+		sendResult();
+		return;
+	}
+	if (!defined("COUNTER_LOG")) {
+		// ログの出力先が設定されていない
+		sendResult();
+		return;
+	}		
+	
+	ini_set("date.timezone", "Asia/Tokyo");
+	$outputData = array();
+	
+	$today = date("Ymd");
+	$yesterday =date("Ymd",strtotime("-1 days"));
+	$totalNum = 1;
+	$todayNum = 1;
+	$yesterdayNum = 0;
+	if (!file_exists(COUNTER_LOG)) {
+		// 初期状態
+		$results = array("total"=>$totalNum,$today=>$todayNum,$yesterday=>0);
+		if (file_put_contents(COUNTER_LOG,json_encode($results)) === FALSE) {
+			// ログの出力が行えない
+			sendResult();
+			return;
+		}
+	} else {
+		$fp = fopen(COUNTER_LOG,"r+");
+		if ($fp && flock($fp,LOCK_EX)) {
+			// ファイルからデータを読み込み
+			$data = fread($fp,1024);
+			$results = json_decode($data,true);
+			// データを更新して書き込み
+			$totalNum = $results["total"] + 1;
+			if (isset($results[$today])) {
+				$todayNum = $results[$today] + 1;
+			}
+			if (isset($results[$yesterday])) {
+				$yesterdayNum = $results[$yesterday];
+			}
+			$newRecord = array(
+				"total"=>$totalNum,
+				$today=>$todayNum,
+				$yesterday=>$yesterdayNum
+			);
+			rewind($fp);
+			$fileSize = fwrite($fp,json_encode($newRecord));
+			ftruncate($fp,$fileSize);
+			flock($fp, LOCK_UN);
+		} else {
+			// ログの出力が行えない
+			sendResult();
+		}
+	}
+	$results = array(
+		"total" => $totalNum,
+		"today" => $todayNum,
+		"yesterday" => $yesterdayNum
+	);
+	$outputData["results"] = &$results;
+	
+	sendResult($outputData);
+}
+/**
+ * JSONデータを送信
+ * @param array $result 送信する処理結果
+ */
+function sendResult(&$result = NULL) {
+	header('Content-type: application/json; charset=utf-8');
+	header('Access-Control-Allow-Origin: *');
+	if ($result != NULL) {
+		$jsonData = json_encode($result);
+		echo $jsonData;
+	} else {
+		echo "{}";
+	}
+}
